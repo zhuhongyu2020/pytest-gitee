e3bf9178d5c719956c61e439c074cac82e16e10f
朱宏玉 <zhuhongyu.2009@163.com>
2022-06-19 12:31:19 +0800

    添加考勤台账

--- test_case/Attendance/test_get_attendance_record.py
+++ test_case/Attendance/test_get_attendance_record.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time   : 2022-06-19 12:22:30
+# @Author : 朱宏玉
+
+
 import allure
 import pytest
 from common.setting import ConfigHandler
@@ -7,26 +13,27 @@ from utils.requestsUtils.requestControl import RequestControl
 from utils.readFilesUtils.regularControl import regular
 from utils.requestsUtils.teardownControl import TearDownHandler
 
-TestData = CaseData(ConfigHandler.data_path + r'UserInfo/get_user_info.yaml').case_process()
+
+TestData = CaseData(ConfigHandler.data_path + r'Attendance/get_attendance_record.yaml').case_process()
 re_data = regular(str(TestData))
 
 
-@allure.epic("开发平台接口")
-@allure.feature("个人信息模块")
-class TestGetUserInfo:
+@allure.epic("云业务平台接口")
+@allure.feature("考勤管理模块")
+class TestGetAttendanceRecord:
 
-    @allure.story("个人信息接口")
+    @allure.story("考勤台账-管理员")
     @pytest.mark.parametrize('in_data', eval(re_data), ids=[i['detail'] for i in TestData])
-    def test_get_user_info(self, in_data, case_skip):
+    def test_get_attendance_record(self, in_data, case_skip):
         """
         :param :
         :return:
         """
         res = RequestControl().http_request(in_data)
         TearDownHandler().teardown_handle(res)
-        Assert(in_data['assert']).assert_equality(response_data=res['response_data'],
+        Assert(in_data['assert']).assert_equality(response_data=res['response_data'], 
                                                   sql_data=res['sql_data'], status_code=res['status_code'])
 
 
 if __name__ == '__main__':
-    pytest.main(['test_get_user_info.py', '-s', '-W', 'ignore:Module already imported:pytest.PytestWarning'])
+    pytest.main(['test_get_attendance_record.py', '-s', '-W', 'ignore:Module already imported:pytest.PytestWarning'])
